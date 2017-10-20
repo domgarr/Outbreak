8b8c653faa7f3caa4b2b80f496117d44ef732579
Domenic Garreffa <garreffd@uwindsor.ca>
2017-10-20 12:34:53 -0400

    Initial LibGDX project starting point

---
+++ desktop/src/com/garreffd/outbreak/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.garreffd.outbreak.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.garreffd.outbreak.OutbreakGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new OutbreakGame(), config);
+	}
+}
